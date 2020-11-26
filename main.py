1103f1bc1a3361fe07fbfa407c31ea56d159976d
Ljw <ljws93@naver.com>
2020-11-27 08:27:12 +0900

    added driver.quit()

--- main.py
+++ main.py
@@ -8,33 +8,56 @@ import DollarIndex
 from datetime import datetime
 import ThreeMain_USA_Flow
 import SectorCheck
+from selenium import webdriver
 
-print("실시 날짜: " + str(datetime.now().strftime("%Y/%m/%d, %H:%M")))
+try:
+    path = "C:/selenium/chromedriver"
 
-# 미국 주요지수
-ThreeMain_USA_Flow.three_main_Flow()
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    options.add_argument("disable-gpu")
 
-# 미국 선물지수
-USA_index_future.indexOfFuture()
+    driver = webdriver.Chrome("C:/selenium/chromedriver", options=options)
 
-# 미국 주요섹터(현재 기준: 1.3)
-SectorCheck.sectorCheck()
 
-# 변동성 지수
-VIX.VIX_Graph()
+    print("실시 날짜: " + str(datetime.now().strftime("%Y/%m/%d, %H:%M")))
 
-# 한국 지수
-EWY.EWY_Graph()
+    # 미국 주요지수
+    ThreeMain_USA_Flow.three_main_Flow()
 
-# 미국 주택가격 지수
-Estate_price.estate_price()
+    print("------------------------")
 
-# 미국 실업수당 지수
-Unemployed.unemployment()
+    # 미국 선물지수
+    USA_index_future.indexOfFuture()
 
-# 한국 경상수지 지수
-AbroadTrading.tradingGraph()
+    print("------------------------")
 
-# 미국 달러 인덱스 지수
-DollarIndex.dollarIndex()
+    # 미국 주요섹터(현재 기준: 1.3)
+    SectorCheck.sectorCheck()
+
+    print("------------------------")
+
+
+    # 변동성 지수
+    VIX.VIX_Graph()
+
+    # 한국 지수
+    EWY.EWY_Graph()
+
+    # 미국 주택가격 지수
+    Estate_price.estate_price()
+
+    # 미국 실업수당 지수
+    Unemployed.unemployment()
+
+    # 한국 경상수지 지수
+    AbroadTrading.tradingGraph()
+
+    # 미국 달러 인덱스 지수
+    DollarIndex.dollarIndex()
+
+    driver.quit()
+
+except:
+    driver.quit()
 
